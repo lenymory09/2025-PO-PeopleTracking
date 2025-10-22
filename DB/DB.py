d94eddfad4b2215e1d1cda258c1f7734fcfa6b40
Dario Chapuis <dario.chapuis@divtec.ch>
2025-10-22 14:06:33 +0200

    Update DB.py
    création d'une class

--- DB/DB.py
+++ DB/DB.py
@@ -1,77 +1,81 @@
 import mysql.connector
 
-def connect_db():
-    # Connexion à la base de données
-    conn = mysql.connector.connect(
-        host="localhost",      # ou l'IP de ton serveur MySQL
-        user="root",
-        password="Admlocal1",
-        database="IA_DB"
-    )
+class DB:
+    def __init__(self):
+        self.connect_db()
 
-# créer la base de données
-def create_db():
-    # Créer un curseur pour exécuter les requêtes
-    conn = mysql.connector.connect(
-        host="localhost",      # ou l'IP de ton serveur MySQL
-        user="root",
-        password="Admlocal1"
-    )
-    cursor = conn.cursor()
-    
     # créer la base de données
-    cursor.execute("""        
-                    -- Création de la base de données
-                    CREATE DATABASE IA_DB;
-                    USE IA_DB; 
-                        -- Création de la table personne
-                    CREATE TABLE personne (
-                        ID_pers INT AUTO_INCREMENT PRIMARY KEY,
-                        ID_personne INT NOT NULL UNIQUE
-                    );
+    def create_db(self):
+        # Créer un curseur pour exécuter les requêtes
+        conn_root = mysql.connector.connect(
+            host="localhost",      # ou l'IP de ton serveur MySQL
+            user="root",
+            password="Admlocal1"
+        )
+        cursor_root = conn_root.cursor()
+        
+        # créer la base de données
+        cursor_root.execute("""        
+                        -- Création de la base de données
+                        CREATE DATABASE IA_DB;
+                        USE IA_DB; 
+                            -- Création de la table personne
+                        CREATE TABLE personne (
+                            ID_pers INT AUTO_INCREMENT PRIMARY KEY,
+                            ID_personne INT NOT NULL UNIQUE
+                        );
 
-                    -- Création de la table lieux
-                    CREATE TABLE lieux (
-                        ID_lieux INT AUTO_INCREMENT PRIMARY KEY,
-                        lieux VARCHAR(50) NOT NULL
-                    );
+                        -- Création de la table lieux
+                        CREATE TABLE lieux (
+                            ID_lieux INT AUTO_INCREMENT PRIMARY KEY,
+                            lieux VARCHAR(50) NOT NULL
+                        );
 
-                    -- Création de la table visiter (relation N-N entre personne et lieux)
-                    CREATE TABLE visiter (
-                        ID_personne INT NOT NULL,
-                        ID_lieux INT NOT NULL,
-                        date_visite DATETIME NOT NULL DEFAULT NOW(),
-                        
-                        PRIMARY KEY (ID_personne, ID_lieux, date_visite),
-                        
-                        CONSTRAINT fk_visiter_personne FOREIGN KEY (ID_personne) REFERENCES personne(ID_pers)
-                            ON DELETE CASCADE
-                            ON UPDATE CASCADE,
-                        CONSTRAINT fk_visiter_lieux FOREIGN KEY (ID_lieux) REFERENCES lieux(ID_lieux)
-                            ON DELETE CASCADE
-                            ON UPDATE CASCADE
-                    );""")
-    # Sauvegarder les changements
-    conn.commit()
-    cursor.close() 
-    conn.close()
+                        -- Création de la table visiter (relation N-N entre personne et lieux)
+                        CREATE TABLE visiter (
+                            ID_personne INT NOT NULL,
+                            ID_lieux INT NOT NULL,
+                            date_visite DATETIME NOT NULL DEFAULT NOW(),
+                            
+                            PRIMARY KEY (ID_personne, ID_lieux, date_visite),
+                            
+                            CONSTRAINT fk_visiter_personne FOREIGN KEY (ID_personne) REFERENCES personne(ID_pers)
+                                ON DELETE CASCADE
+                                ON UPDATE CASCADE,
+                            CONSTRAINT fk_visiter_lieux FOREIGN KEY (ID_lieux) REFERENCES lieux(ID_lieux)
+                                ON DELETE CASCADE
+                                ON UPDATE CASCADE
+                        );""")
+        # Sauvegarder les changements
+        conn_root.commit()
+        cursor_root.close() 
+        conn_root.close()
 
-# insérer une personne dans la base de données 
-def insert_personne_passage(ID_personne, ID_lieux):
-    # Créer un curseur pour exécuter les requêtes
-    cursor = conn.cursor()
-    
-    # Insérer la personne
-    cursor.execute("INSERT INTO personne (ID_personne) VALUES (%s)", (ID_personne,))
-    
-    # Insérer le passage dans la table visiter avec la date actuelle
-    cursor.execute("""
-        INSERT INTO visiter (ID_personne, ID_lieux, date_visite)
-        VALUES (%s, %s, NOW())
-    """, (ID_personne, ID_lieux))
-    
-    # Sauvegarder les changements
-    conn.commit()
+
+    def connect_db(self):
+        # Connexion à la base de données
+        self.conn = mysql.connector.connect(
+            host="localhost",      # ou l'IP de ton serveur MySQL
+            user="root",
+            password="Admlocal1",
+            database="IA_DB"
+        )
+        self.cursor = conn.cursor()
+
+
+    # insérer une personne dans la base de données 
+    def insert_personne_passage(self, ID_personne, ID_lieux):
+        # Insérer la personne
+        self.cursor.execute("INSERT INTO personne (ID_personne) VALUES (%s)", (ID_personne,))
+        
+        # Insérer le passage dans la table visiter avec la date actuelle
+        self.cursor.execute("""
+            INSERT INTO visiter (ID_personne, ID_lieux, date_visite)
+            VALUES (%s, %s, NOW())
+        """, (ID_personne, ID_lieux))
+        
+        # Sauvegarder les changements
+        self.conn.commit()
 
 # # Exemple : lire des données
 # cursor.execute("SELECT * FROM users")
@@ -80,7 +84,8 @@ def insert_personne_passage(ID_personne, ID_lieux):
 # for row in rows:
 #     print(row)
 
-def close_db():
+
+def close_db(self):
     # Fermer le curseur et la connexion
-    cursor.close() 
-    conn.close()
+    self.cursor.close() 
+    self.conn.close()
